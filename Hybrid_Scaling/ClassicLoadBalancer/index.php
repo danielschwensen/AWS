9f044fea5af79d44bb79a637b40eedb548051f40
Daniel Schwensen <daniel.schwensen@gmail.com>
2019-11-03 13:46:48 +0100

    bootstrap2

---
+++ Hybrid_Scaling/ClassicLoadBalancer/index.php
@@ -0,0 +1,15 @@
+<html>
+<title> LA CLB DEMO </title>
+<body>
+<h1>
+<center>
+<h1>Las Vegas 2008</h1>
+<IMG SRC="LV2008.jpg" ALT="Las Vegas 2008"><br>
+<?php
+$eip = file_get_contents('http://169.254.169.254/latest/meta-data/public-ipv4');
+echo $eip;
+?>
+</center>
+</h1>
+</body>
+</html>
