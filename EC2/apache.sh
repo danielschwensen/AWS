00ce08249d3aacffb1ee635b39420820498d2cb7
Daniel Schwensen <36114476+danielschwensen@users.noreply.github.com>
2021-08-16 22:23:55 +0200

    move index.html

--- EC2/apache.sh
+++ EC2/apache.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 yum update -y
 yum install -y httpd
-
 instanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-echo "<h1>Hello World from $instanceId</h1>" > /var/www/html/index.html
-
+echo "<h1>Hello World from $instanceId</h1>" > index.html
+mv index.html /var/www/html/
 systemctl start httpd
 systemctl enable httpd
